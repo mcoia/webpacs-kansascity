d735daeccf74feaf28961721352821984559f573
hammondjn <jessica@mobiusconsortium.org>
2012-03-08 09:27:50 -0600

    Adjusted the redirect message to reflect the user's scope

--- domainredirect.js
+++ domainredirect.js
@@ -24,8 +24,27 @@ if(href.indexOf("missouri.edu") != -1 ){
 }else{}
 
 
+var s = /searchscope=\d*/
+var t = /~S\d*/
+if (href.match(s)){
+  var searchscope = href.match(s)
+  var scope = searchscope[0].substring(12);
+}else if (href.match(t)){
+  var tilde = href.match(t)
+  var scope = tilde[0].substring(2);
+}else {
+  var scope;
+}
+
+if (scope == undefined || scope == 0){
+  var newURL = searchmobius
+}else {
+  var newURL = searchmobius + "/search~S" + scope
+}
+
 if (href.indexOf("&redirected") != -1 ) {
-    var redirectMessage = "<p>You've been redirected from wilo.mobius.umsystem.edu or wilo.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://kansascity.searchmobius.org'>http://kansascity.searchmobius.org</a>.</p>"
+    
+    var redirectMessage = "<p>You've been redirected from kansascity.mobius.umsystem.edu or kansascity.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
 
     $(document).ready(function () {
         $('#alertMessage').html(redirectMessage);
